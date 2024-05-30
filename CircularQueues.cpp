35d6f0c4c1980d912cd0866bfe778399090dde11
anobee <HP@DESKTOP-KFAVCD8>
2024-05-30 15:46:48 +0700

    selesai array

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -114,6 +114,33 @@ int main() {
             cout << "enter your choice (1-4):";
             cin >> ch;
             cout << endl;
+
+            switch (ch){
+                case '1': {
+                    q.insert();
+                    break;
+                }
+                case '2': {
+                    q.remove();
+                    break;
+                }
+                case '3': {
+                    q.display();
+                    break;
+                }
+                case '4': {
+                    return 0;
+                }
+
+                default: {
+                    cout << "invalid option!!" << endl;
+                    break;
+                }
+            }
+        }
+        catch (exception& e){
+            cout << "check for the values entered." << endl;
         }
     }
+    return 0;
 }
