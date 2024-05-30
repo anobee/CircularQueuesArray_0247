5b79de3ea20090ab184504db8e574c4c4ddfbbb5
anobee <HP@DESKTOP-KFAVCD8>
2024-05-30 14:41:57 +0700

    membuat acces modifier public pada class queues

---
+++ CircularQueues.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class queues {
+    private :
+     static const int max =3;
+     int front, rear;
+     int queue_array [max];
+     
+    public :
+        queues() {
+            front = -1;
+            rear = -1;
+        }
